525566685f64fd02698b4dfffbbd1581b1847588
rulibelajar <rulixies@gmail.com>
2025-07-13 10:54:49 +0700

    sebelum CRUD

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,7 @@ Route::get('/', function () {
     ]);
 });
 
-// saya comment dulu karena sudah memakai yang dibawah
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
